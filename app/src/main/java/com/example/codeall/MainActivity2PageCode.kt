8f324107343cf3312223c382b2f5d2f5a25d3901
zara <mamasydykova_g@auca.kg>
2021-12-11 21:54:27 +0600

    First 3 pages

---
+++ app/src/main/java/com/example/codeall/MainActivity2PageCode.kt
@@ -0,0 +1,11 @@
+package com.example.codeall
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2PageCode : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_activity2_page_code)
+    }
+}
---
+++ app/src/main/java/com/example/codeall/MainActivity3PageCode.kt
@@ -0,0 +1,11 @@
+package com.example.codeall
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity3PageCode : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_activity3_page_code)
+    }
+}
